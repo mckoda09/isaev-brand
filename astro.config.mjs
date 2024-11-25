4ed9f6de03fec5e6d0912ba45e938a54e94f1902
mckoda09 <mckoda09@gmail.com>
2024-11-25 20:52:01 +0500

    made all

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind()],
+  devToolbar: {
+    enabled: false,
+  },
+});
---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+      colors: {
+        primary: "#374aa8",
+        text: "#161a38",
+      },
+    },
+  },
+  plugins: [],
+};
